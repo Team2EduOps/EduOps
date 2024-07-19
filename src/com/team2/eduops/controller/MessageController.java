9cd1cadcbb1b719f13c51ae88eb7440a2a88895b
phantom-black <black_phantom@naver.com>
2024-07-19 15:26:47 +0900

    Refactor "LoginController의 studentLogin()를 userLogin()으로 수정"
    - LoginController의 studentLogin()과 adminLogin() 메소드의 유사성이 높아
    userLogin()으로 통합
    - 그 과정에서 상기 메소드들이 사용되는 PageController의 runPage() 코드 수정
    - 또 중복되는 리터럴 값들을 상수 처리하기 위해 model 패키지의 StudentVO, AdminVO에 상수를 추가하고
    getter 코드 추가
    - controller 패키지 아래에 MessageController를 추가: CLI 프로그래밍 중에 발생하는 콘솔 창 출력용
    메소드들을 취합할 예정

---
+++ src/com/team2/eduops/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.team2.eduops.controller;
+
+public class MessageController {
+//	public void showEntrancePage() {
+//		// 로그인회원가입 페이지 띄우기
+//		System.out.println("===로그인/회원가입 페이지===");
+//		System.out.println("1. 학생 로그인");
+//		System.out.println("2. 관리자 로그인");
+//		System.out.println("3. 학생 회원가입");
+//		System.out.println("0. 프로그램 종료");
+//	}
+//	
+//	public void showStudentLoginPage() {
+//		System.out.println("===1. 학생 로그인===");
+//		System.out.println("안녕하세요 :)");
+//	}
+}

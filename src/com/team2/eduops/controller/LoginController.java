d4ea58458769824700166fc3716de5b4f6233989
phantom-black <black_phantom@naver.com>
2024-07-19 11:39:31 +0900

    Feat "프로그램 전체 흐름 관련 코드 작성"
    - MainEntry 안 main 메소드에 프로그램 실행 코드 추가
    - PageController 안 프로그램 실행 메소드 및 관련 메소드 구조 초기 세팅
    - LoginController와 JoinController에 runPage()에 필요한 메소드 추가(진행중)

--- src/com/team2/eduops/controller/LoginController.java
+++ src/com/team2/eduops/controller/LoginController.java
@@ -1,7 +1,27 @@
 package com.team2.eduops.controller;
 
-
 // 로그인 기능 관련 메소드들
 public class LoginController {
 
+	public int studentLogin() {
+		int studentNo = -1;
+
+		return studentNo;
+
+	}
+
+	public int adminLogin() {
+		int adminNo = -1;
+
+		return adminNo;
+
+	}
+
+	public boolean checkLogin(int userNo) {
+		if (userNo == -1) {
+			System.out.println("오류가 발생했습니다. 다시 로그인해주세요.");
+			return false;
+		}
+		return true;
+	}
 }
--- src/com/team2/eduops/view/LoginPage.java
+++ src/com/team2/eduops/view/LoginPage.java
@@ -1,6 +1,7 @@
 package com.team2.eduops.view;
 
+
 // 프로그램 시작 시 띄워지는 로그인/회원가입 페이지 관련 메소드, gui
 public class LoginPage {
-
+	
 }

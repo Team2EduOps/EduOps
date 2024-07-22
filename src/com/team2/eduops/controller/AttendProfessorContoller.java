b2f24175604585296ec6450fbe1e59247429b642
bkjeong <qaz2342004@naver.com>
2024-07-22 22:12:21 +0900

    드

---
+++ src/com/team2/eduops/controller/AttendProfessorContoller.java
@@ -0,0 +1,11 @@
+package com.team2.eduops.controller;
+
+public class AttendProfessorContoller {
+    //lookupAttendance -교수님 메뉴 ->학생관리 -> 1. 학생 보기
+
+
+    //approveVacation-교수님 메뉴 ->학생관리 -> 2. 휴가 승인
+
+
+
+}

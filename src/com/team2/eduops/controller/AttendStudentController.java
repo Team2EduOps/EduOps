bef0b0d7b7b269625b66478e847de78c4e116ea1
bkjeong <qaz2342004@naver.com>
2024-07-23 15:38:54 +0900

    fix CheckInOut

--- src/com/team2/eduops/controller/AttendStudentController.java
+++ src/com/team2/eduops/controller/AttendStudentController.java
@@ -15,7 +15,7 @@ import com.team2.eduops.dbConn.util.*;
 public class AttendStudentController {
     static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     //근태 관리 메뉴 동작
-   public static void attendMenu(int stdno) throws ClassNotFoundException, SQLException, ParseException {
+    public static void attendMenu(int stdno) throws ClassNotFoundException, SQLException, ParseException {
         boolean backpage = true;
         while (backpage) {
             showAttendMenu();  // 메뉴 보이기
@@ -91,13 +91,13 @@ public class AttendStudentController {
     } //end
 
     //5-3.누적 지원금 메뉴 : showCashMenu
-     public static void showCashMenu(){
+    public static void showCashMenu(){
         System.out.println("\n------5-3.누적 지원금 조회-------");
         System.out.println("누적 지원금 조회페이지입니다.");
         System.out.println("\t 1. 현재 누적 지원금");
         System.out.println("\t 2. 지난 지원금: 월별");
-         System.out.println("\t 뒤 페이지 이동: 0");
-         System.out.println("\t >> 원하는 메뉴 선택 하세요.   ");
+        System.out.println("\t 뒤 페이지 이동: 0");
+        System.out.println("\t >> 원하는 메뉴 선택 하세요.   ");
     }//end
 
     //날짜 형식 맞는지 확인 메소드
@@ -157,8 +157,8 @@ public class AttendStudentController {
             }else {
                 System.out.println("\t 올바른 형식이 아닙니다.. 다시 입력해 주세요.");
             }//if end
-            }
-        }//while end
+        }
+    }//while end
 
     //년 월 형식 맞는지 확인 메소드
     public static boolean isValidYearMonth(String date){
@@ -173,43 +173,43 @@ public class AttendStudentController {
     }
 
     //5-2. 월별 조회(근태 page):lookupMonthly
-     public static void lookupMonthly(int stdno) throws SQLException {
+    public static void lookupMonthly(int stdno) throws SQLException {
         boolean bool=true;
-         while(bool){
-             System.out.print("\t YYYY-MM 형식 입력: ");
-             String inputMonth= ConnectController.scanData();
+        while(bool){
+            System.out.print("\t YYYY-MM 형식 입력: ");
+            String inputMonth= ConnectController.scanData();
 
-             if(isValidYearMonth(inputMonth)){ //맞는 형식으로 입력!
-                 String firstdate=inputMonth+"-01";
-                 String lastdate=inputMonth+"-31";
-                 String sql = "SELECT TO_CHAR(ATTEND_DATE,'YYYY-MM-DD') ,ATTEND_STATUS " +
-                         "FROM ATTENDANCE WHERE ATTEND_DATE BETWEEN TO_DATE('" +firstdate+"','YYYY-MM-DD') AND TO_DATE('"+lastdate+"','YYYY-MM-DD') "+
-                         "AND STD_NO = "+stdno;
+            if(isValidYearMonth(inputMonth)){ //맞는 형식으로 입력!
+                String firstdate=inputMonth+"-01";
+                String lastdate=inputMonth+"-31";
+                String sql = "SELECT TO_CHAR(ATTEND_DATE,'YYYY-MM-DD') ,ATTEND_STATUS " +
+                        "FROM ATTENDANCE WHERE ATTEND_DATE BETWEEN TO_DATE('" +firstdate+"','YYYY-MM-DD') AND TO_DATE('"+lastdate+"','YYYY-MM-DD') "+
+                        "AND STD_NO = "+stdno;
 
-                 PreparedStatement pstmt = ConnectController.getPstmt(sql);
-                 ResultSet rs = ConnectController.executePstmtQuery(pstmt);
-                 ResultSetMetaData metaData =rs.getMetaData();
-                 //case1- DB에 없는 값, 튜플 X
-                 if (!rs.isBeforeFirst()) {
-                     System.out.println("\t 해당 월에 데이터가 없습니다.");
-                 } else {
-                     System.out.println("\t Attend_status= 0:결석,1:출석,2:공가,3:지각,4:조퇴");
-                     while (rs.next()) {
-                         Object columnValue = rs.getObject("Attend_status");
-                         // case2- DB에 튜플 O, 상태 O
-                             System.out.print("\t 날짜: " + rs.getString(1));
-                             System.out.println("\t Attend_status: " + rs.getInt(2));
+                PreparedStatement pstmt = ConnectController.getPstmt(sql);
+                ResultSet rs = ConnectController.executePstmtQuery(pstmt);
+                ResultSetMetaData metaData =rs.getMetaData();
+                //case1- DB에 없는 값, 튜플 X
+                if (!rs.isBeforeFirst()) {
+                    System.out.println("\t 해당 월에 데이터가 없습니다.");
+                } else {
+                    System.out.println("\t Attend_status= 0:결석,1:출석,2:공가,3:지각,4:조퇴");
+                    while (rs.next()) {
+                        Object columnValue = rs.getObject("Attend_status");
+                        // case2- DB에 튜플 O, 상태 O
+                        System.out.print("\t 날짜: " + rs.getString(1));
+                        System.out.println("\t Attend_status: " + rs.getInt(2));
 
-                             // case3-오늘 퇴실 처리 X ,튜플 O, 상태 X
-                             if (columnValue == null) {
-                                 System.out.println("\t 오늘은 퇴실 처리를 하지 않아 조회가 불가합니다.");
-                             } //if end
-                         }//for end
-                         bool = false;
-                     } //내부 while end
-                 }else System.out.println("\t 올바른 형식이 아닙니다.. 다시 입력해 주세요.");
-             }
-         }
+                        // case3-오늘 퇴실 처리 X ,튜플 O, 상태 X
+                        if (columnValue == null) {
+                            System.out.println("\t 오늘은 퇴실 처리를 하지 않아 조회가 불가합니다.");
+                        } //if end
+                    }//for end
+                    bool = false;
+                } //내부 while end
+            }else System.out.println("\t 올바른 형식이 아닙니다.. 다시 입력해 주세요.");
+        }
+    }
     //applyVacation- 휴가 신청(근태 page)
 
     public static void applyVacation (int stdno){
@@ -227,10 +227,9 @@ public class AttendStudentController {
     }
 
     public static void main(String[] args) throws SQLException, ParseException, ClassNotFoundException {
-     attendMenu(1);
+        attendMenu(1);
     }
 }
-
 
 
 
--- src/com/team2/eduops/controller/CheckInOut.java
+++ src/com/team2/eduops/controller/CheckInOut.java
@@ -2,45 +2,98 @@ package com.team2.eduops.controller;
 
 import java.sql.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /*
-public static void class CheckInOut {
-    Connection conn = DriverManager.getConnection(url,user,password);
-    PreparedStatement pstmt = conn
-        //입실
-        public static void checkIn(int stdno) throws SQLException,ClassNotFoundException {
-            System.out.println("입실이 완료되었습니다.");
-            LocalDate today = LocalDate.now();
-            Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+**********학생메뉴_1.입실 1.퇴실 메뉴 ************
+*/
 
-            DateTimeFormatter formattedtime = DateTimeFormatter.ofPattern("HH:mm:ss");
+public class CheckInOut{
+    LocalDate today;
+    Date sqlDate;
+        // localdate(현재 날짜) 를 java.sql.Date로 변환
+    LocalDateTime localDateTime;
+    Timestamp timestamp;
+        // LocalDateTime을 java.sql.Timestamp로 변환
 
+    PreparedStatement pstmt;
+    ResultSet rs;
 
-            pstmt = conn. prepareStatement("INSERT INTO ATTENDANCE VALUES( ?,?,?)");
-            pstmt.setDate(2, today);
-            pstmt.setInt(3, stdno);
-            //update
-            pstmt = conn.prepareStatement("UPDATE ATTENDANCE set CI_TIME = "+ currentTimestamp + " where STD_NO = "+ std_no +" and ATTEND_DATE = "+today);
+    //********입실 명단을 list에 추가 +DB에 삽입********
+    public void update(int stdno) throws SQLException {
+        today = LocalDate.now();
+        sqlDate = Date.valueOf(today);
+        localDateTime = LocalDateTime.now();
+        timestamp = Timestamp.valueOf(localDateTime);
+
+        System.out.println("\t 입실이 완료되었습니다.: "+ today);
+        String sql = "INSERT INTO ATTENDANCE (std_no,attend_date, ci_time) VALUES (?,?,?)";
+        pstmt = ConnectController.getPstmt(sql);
+        pstmt.setInt(1,stdno);
+        pstmt.setDate(2,sqlDate);
+        pstmt.setTimestamp(3,timestamp);
     }
 
-    //퇴실
-    public static void checkOut(int stdno) throws SQLException,ClassNotFoundException{
-        System.out.pirntln("퇴실이 완료되었습니다.");
-        LocalDate today = LocalDate.now();
-        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+    //*********퇴실시 명단에서 삭제+DB에 update(CO_time과 Attend_status)**********
+    public void delete(int stdno) throws SQLException {
+        today = LocalDate.now();
+        sqlDate = Date.valueOf(today);
+        localDateTime = LocalDateTime.now();
+        timestamp = Timestamp.valueOf(localDateTime);
+        LocalTime cIt=null;
+        LocalTime cOt=null;
+        LocalTime entryTime=null;
+        LocalTime exitTime=null;
 
-        DateTimeFormatter formattedtime = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("\t 퇴실이 완료되었습니다.: "+ today);
+        String sql = "UPDATE ATTENDANCE set CO_TIME = "+timestamp+ " where STD_NO = "+stdno+"and ATTEND_DATE = "+sqlDate;
+        pstmt = ConnectController.getPstmt(sql);
 
-        pstmt = conn.prepareStatement("UPDATE ATTENDANCE set CO_TIME = "+currentTimestamp+ " where STD_NO = "+stdno+"and ATTEND_DATE = "+today);
+        //*******0:결석(default), 1: 출석, 2: 공가, 3: 지각 ,4: 조퇴**********
+        sql = "SELECT CI_TIME,CO_TIME from ATTENDANCE where std_no = "+stdno+ " AND ATTEND_DATE="+sqlDate;
+        pstmt = ConnectController.getPstmt(sql);
+        rs = ConnectController.executePstmtQuery(pstmt);
+        if(rs.next()){
+            cIt = rs.getTimestamp("CI_TIME").toLocalDateTime().toLocalTime();
+            cOt = rs.getTimestamp("CO_TIME").toLocalDateTime().toLocalTime();
+                //비교시간 :9시와 18시
+            entryTime = LocalTime.of(9,0);
+            exitTime = LocalTime.of(18,0);
+        }
 
-        public static void main(String[] args){
+        int attendStatus = 0; //결석:0 // 공가:1
+        if(cIt.isBefore(entryTime)&&cOt.isAfter(exitTime)){
+            attendStatus=1;  // 지각:3
+        }else if(cIt.isAfter(entryTime)&&cOt.isAfter(exitTime)){
+            attendStatus=3;  // 조퇴:4
+            }else{  attendStatus=4;}
+        //attendStatus update
+        sql = "UPDATE ATTENDANCE set ATTEND_STATUS = "+attendStatus+ " where STD_NO = "+stdno+" and ATTEND_DATE = "+sqlDate;
+        pstmt = ConnectController.getPstmt(sql);
+    }
 
+    //********최종: 명단에 있는지 확인하여, 입/퇴실 여부 출력 :********
+    public void checkIO(int stdno) throws SQLException {
+
+        sqlDate = Date.valueOf(LocalDate.now());
+        String sql = "SELECT * from ATTENDANCE where std_no = "+stdno+ " AND ATTEND_DATE = "+sqlDate;
+        pstmt = ConnectController.getPstmt(sql);
+        rs = ConnectController.executePstmtQuery(pstmt);
+
+        if (!rs.isBeforeFirst()) { //값이 없으면, 입실 생성
+            System.out.println("\t 1. 입실");
+            update(stdno);
+        } else { //값이 있으면, 퇴실 생성
+            System.out.println("\t 1. 퇴실");
+            delete(stdno);
         }
     }
+    public void main(String[] args) throws SQLException {
+        checkIO(1);
+    }
 }
-*/
+
 

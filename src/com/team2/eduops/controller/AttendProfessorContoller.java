3e84e49ccf77c26347c1a2fff7dc022d823dcf1b
bkjeong <qaz2342004@naver.com>
2024-07-24 09:30:16 +0900

    #fix AttendProfessorContoller 완료

--- src/com/team2/eduops/controller/AttendProfessorContoller.java
+++ src/com/team2/eduops/controller/AttendProfessorContoller.java
@@ -3,6 +3,7 @@ package com.team2.eduops.controller;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -34,5 +35,18 @@ public class AttendProfessorContoller {
             e.printStackTrace();
         }
     }
-    //approveVacation-교수님 메뉴 ->학생관리 -> 2. 휴가 승인
+
+    //*********approveVacation-교수님 메뉴 ->학생관리 -> 2. 휴가 승인 -> 출석상태 : 공가로 변경********
+    public static void updateAttendance(String stdNo, Date attendDate) throws SQLException {
+        String updateSQL = "UPDATE attendance SET status = ? WHERE std_no = ? AND attend_date = ?";
+        PreparedStatement pstmt = ConnectController.getPstmt(updateSQL);
+        ResultSet rs = ConnectController.executePstmtQuery(pstmt);
+            // PreparedStatement의 파라미터 설정
+            pstmt.setInt(1, 2); // 근태 상태를 2로 설정
+            pstmt.setString(2, stdNo);
+            pstmt.setDate(3, attendDate);
+            // 쿼리 실행
+            int rowsAffected = ConnectController.executePstmtUpdate(pstmt);
+            System.out.println(rowsAffected + ": 휴가 승인");
+    }
 }
